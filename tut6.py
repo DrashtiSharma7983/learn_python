2bbfeed4fc5dfa5bcca08b08325c22b8f77fff2d
Drashti Sharma <86773589+DrashtiSharma7983@users.noreply.github.com>
2021-08-27 21:32:13 +0530

    Add files via upload

---
+++ tut6.py
@@ -0,0 +1,8 @@
+#single line comment
+"""
+multi
+line
+comment
+"""
+print("Code with\n Drashti","i like\t coding") #\t for tab
+print("C:\'narry")
---
+++ tut7.py
@@ -0,0 +1,24 @@
+var1="hello drashti"
+var2=4
+var3=45.2
+
+
+print(var1)
+print(type(var1))#for printing the type of variable
+print(type(var2))
+print(type(var3))
+
+
+#for user input
+#print("please enter your number")
+#num=input() #thr number which entered by user is string
+#print(int(num)+10)
+
+#quix for adding two numbers
+
+x=int(input("enter your first number"))
+
+y=int(input("enter your second number"))
+sum=(x + y)
+
+print(sum)
