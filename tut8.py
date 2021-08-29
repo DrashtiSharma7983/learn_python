cddfd7865fb5258f9b4fcbe65a7d23641bd04cf5
Drashti Sharma <86773589+DrashtiSharma7983@users.noreply.github.com>
2021-08-29 09:12:49 +0530

    Add files via upload

---
+++ tut8.py
@@ -0,0 +1,20 @@
+str1 = "hello i am here to teach you"
+
+print(len(str1))
+print(str1[::-1])  # for reverse the string
+
+print(str1.endswith("you"))
+print(str1.endswith("youa"))
+
+print(str1.count("e"))  # for counting character
+
+print(str1.capitalize())  # for cpitializing the first letter of the string
+
+print(str1.find("o"))
+
+print(str1.endswith("you"))
+
+print(str1.lower())
+
+print(str1.upper())
+print(str1.replace("am","are"))
