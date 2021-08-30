cf85618c055ca500af541570cfa4ff1b7534e435
Drashti Sharma <86773589+DrashtiSharma7983@users.noreply.github.com>
2021-08-30 11:31:32 +0530

    Add files via upload

---
+++ tut10.py
@@ -0,0 +1,26 @@
+family=["ritik","janvi","srashti","drashti","kalpit"]
+
+#print(family)
+numlist=[2,9,5,2,8,1]
+#numlist.sort() #for sorting the number list
+#numlist.reverse() #for reverseing the list
+#print(numlist)
+
+#print(numlist[1:5])
+#print(numlist[::1])
+#print(numlist[::2])
+#print(numlist[::-1])
+
+#print(max(numlist))
+#print(min(numlist))
+#numlist.pop()
+
+numlist.insert(9,10)
+print(numlist)
+
+#for swaping two values
+a=1
+b=4
+a,b=b,a
+print(a,b)
+
---
+++ tut11.py
@@ -0,0 +1,15 @@
+# dictionary is nothing but key value pair
+d1={"drashti":"msc","srashti":"ba","ritik":"coaching"}
+
+#print(d1)
+#print(d1["drashti"])
+
+# for adding one more key value pair
+#d1["kalpit"]="sandwich"
+#print(d1)
+
+#d1["duggu"]="toffee"
+#print(d1)
+#for deleting data from the dictionary
+#del d1["duggu"]
+#print(d1)
