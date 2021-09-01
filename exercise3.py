aac3cb8c5cf351cbb6b079e304dcd35433febafc
Drashti Sharma <86773589+DrashtiSharma7983@users.noreply.github.com>
2021-09-01 12:32:01 +0530

    Add files via upload

---
+++ exercise3.py
@@ -0,0 +1,6 @@
+#guess the number and take input from user
+#number of guesses 9
+#print no. of guesses print
+#number of guesses he took to finish
+#gameover
+
---
+++ tut13.py
@@ -0,0 +1,21 @@
+s=set()
+print(s)
+
+s_from_list=set([1,2,3,4,5])
+print(s_from_list)
+#print(type(s_from_list))
+
+s.union(1,2,3)
+s.intersection(3,4,2)
+'''s.add(1)
+s.add(1)# it will not accept same value
+s.add(2)
+s.remove(1)'''
+print(s)
+
+'''some more function i have to check on the internet
+difference
+symmetric difference
+disjoint set
+
+'''
